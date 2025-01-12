ab4073b6a6950a56a99f1040253418be10bf50d0
walidmadad <walid.madad123@gmail.com>
2025-01-12 03:30:33 +0100

    add FileManager

---
+++ src/main/java/org/example/FileManager.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManager {
+    public static List<String> readLogFile(String filePath) throws IOException {
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath));) {
+            List<String> logs = new ArrayList<>();
+            String line;
+            while((line = br.readLine()) != null){
+                logs.add(line);
+            }
+            return logs;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
