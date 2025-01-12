eb3c22dc48f4e7826f5de127420fd67798e64bbf
walidmadad <walid.madad123@gmail.com>
2025-01-12 16:12:39 +0100

    add class LogEntry

--- src/main/java/org/example/LogParser.java
+++ src/main/java/org/example/LogParser.java
@@ -1,4 +1,21 @@
 package org.example;
 
+import java.util.regex.Pattern;
+
 public class LogParser {
+    private static final Pattern logPattern = Pattern.compile("\\[(.*?)\\] \\[(.*?)\\] (.*)");
+}
+
+class LogEntry{
+    private final String date, type, message;
+
+    public LogEntry(String date, String type, String message){
+        this.date = date;
+        this.type = type;
+        this.message = message;
+    }
+
+    public String getType(){return type;}
+    public String getDate(){return date;}
+    public String getMessage(){return message;}
 }
