25444d5b3f354bda2aee02c71530646dec52b13a
walidmadad <walid.madad123@gmail.com>
2025-01-12 17:25:28 +0100

    Update JavaDoc FileManager.java

--- src/main/java/org/example/FileManager.java
+++ src/main/java/org/example/FileManager.java
@@ -1,39 +1,30 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A utility class for managing file operations.
- *
- * <p>This class provides methods to handle file operations such as reading the contents
- * of a log file and returning its lines as a list of strings.</p>
+ * Cette classe gère la lecture des fichiers log.
  */
 public class FileManager {
 
     /**
-     * Reads a log file from the specified file path and returns its contents as a list of strings.
+     * Lit un fichier log et retourne son contenu sous forme de liste de lignes.
      *
-     * <p>Each line of the log file is added as an element in the returned list.</p>
-     *
-     * @param filePath The path of the log file to be read.
-     * @return A list of strings where each string represents a line from the log file.
-     * @throws IOException If an I/O error occurs while reading the file.
-     * @throws RuntimeException If an error occurs during file reading.
+     * @param filePath Le chemin du fichier log à lire.
+     * @return Une liste de chaînes représentant les lignes du fichier log.
+     * @throws IOException Si une erreur d'entrée/sortie se produit lors de la lecture du fichier.
      */
     public static List<String> readLogFile(String filePath) throws IOException {
-        try(BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            List<String> logs = new ArrayList<>();
+        List<String> logs = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
-            while((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 logs.add(line);
             }
-            return logs;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
+        return logs;
     }
 }
+
